692cdc18a3b7c729d05121ecae1c705af3e536bc
Andrey <andreyshanorm@MacBook-Air-Andrey.local>
2023-12-19 22:37:08 +0400

    create template

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 // @todo: Темплейт карточки
-
+const cardsTemplate = document.querySelector('#card-template').content;
+const cardTemplate = cardsTemplate.querySelector('.places__item')
+console.log(cardTemplate);
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
