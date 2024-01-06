85855fdf5bfe6c37eb0b3255efa1ed8804a5a9a6
Andreyshanorm <gorbachev_71@bk.ru>
2024-01-06 10:58:14 +0400

    last changes

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -23,18 +23,6 @@ const initialCards = [
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
 ];
 
 export { initialCards as cardsArray }
